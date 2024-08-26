07b84a96b899aba7a34d54a5801ccb71598da89f
jncurrea <95834653+jncurrea@users.noreply.github.com>
2024-08-26 16:28:30 -0500

    Visuals

--- main.js
+++ main.js
@@ -34,3 +34,21 @@ document.addEventListener('DOMContentLoaded', function() {
         .catch(error => console.error('Error fetching GitHub repositories:', error));
 });
 */
+
+document.addEventListener('DOMContentLoaded', function() {
+    var divElement1 = document.getElementById('viz1724702839630');
+    var vizElement1 = divElement1.getElementsByTagName('object')[0];
+    vizElement1.style.width = '1000px';  // Adjusted width
+    vizElement1.style.height = '600px';  // Adjusted height
+    var scriptElement1 = document.createElement('script');
+    scriptElement1.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+    vizElement1.parentNode.insertBefore(scriptElement1, vizElement1);
+
+    var divElement2 = document.getElementById('viz1724703197137');
+    var vizElement2 = divElement2.getElementsByTagName('object')[0];
+    vizElement2.style.width = '1000px';  // Adjusted width
+    vizElement2.style.height = '600px';  // Adjusted height
+    var scriptElement2 = document.createElement('script');
+    scriptElement2.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+    vizElement2.parentNode.insertBefore(scriptElement2, vizElement2);
+});
