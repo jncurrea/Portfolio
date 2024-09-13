9bc534e68d0700b8536c35c7860f79c5f8fd64a5
jncurrea <95834653+jncurrea@users.noreply.github.com>
2024-09-13 11:18:53 -0500

    Test and Changes

--- main.js
+++ main.js
@@ -35,34 +35,8 @@ document.addEventListener('DOMContentLoaded', function() {
     scriptElement2.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
     vizElement2.parentNode.insertBefore(scriptElement2, vizElement2);
 
-    //Timeline
-    const timelineData = [
-        { title: 'Master of Science, Business Analytics', description: 'The University of Texas at Austin', date: 'July 2024 - May 2025' },
-        { title: 'Sr. Data Analyst, Digital Channels', description: 'Banco de Bogota - Bogota, Colombia', date: 'Oct 2022 - Jul 2024' },
-        { title: 'Jr. Data Analyst, Supply Chain & Warehouse Operations', description: 'Merqueo - Bogota, Colombia', date: 'Mar 2022 - Oct 2022' },
-        { title: 'Bootcamp, Data Analysis & Visualization', description: 'The University of Texas at Austin - McCombs School of Business', date: 'December 2021 - July 2022' },
-        { title: 'Business Intelligence Analyst', description: 'Puntos Colombia - Bogota, Colombia', date: 'June 2021 - Aug 2021' },
-        { title: 'Business Intelligence Analyst', description: 'SoyYo, Servicios de Identidad Digital - Bogota, Colombia', date: 'June 2020 - Aug 2020' },
-        { title: 'Bachelor of Science, Mathematics & Information Technology', description: 'The University of Texas at Tyler (Honors Program)', date: 'August 2017 - December 2021' }
-    ];
-
-    const timelineContainer = document.querySelector('.timeline');
-
-    timelineData.forEach((event, index) => {
-        const eventHTML = `
-            <div class="timeline-event">
-                <div class="timeline-content">
-                    <h3>${event.title}</h3>
-                    <p>${event.description}</p>
-                    <span class="date">${event.date}</span>
-                </div>
-            </div>
-        `;
-        timelineContainer.innerHTML += eventHTML;
-    });
-
     // Intersection Observer for triggering animations when in view
-    const timelineEvents = document.querySelectorAll('.timeline-event');
+    const timelineEvents = document.querySelectorAll('.timeline2 .container2');
 
     const observerOptions = {
         threshold: 0.3 // Trigger when 30% of the element is visible
@@ -83,5 +57,4 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Show the Overview tab by default
     document.getElementById('overview').classList.add('active');
-    
 });
